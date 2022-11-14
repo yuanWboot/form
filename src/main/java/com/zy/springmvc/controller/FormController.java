f10a4532f23d6e64921cf9e6f0f86cef52b54639
zy <1661996964@qq.com>
2022-11-14 12:40:56 +0800

    设置默认值。@RequestParam(value = "" defaultValue="")

--- src/main/java/com/zy/springmvc/controller/FormController.java
+++ src/main/java/com/zy/springmvc/controller/FormController.java
@@ -3,13 +3,25 @@ package com.zy.springmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FormController {
+//    @PostMapping("/apply")
+//    @ResponseBody
+//    public String apply(String name,String course,Integer[] purpose){
+//        System.out.println(name);
+//        System.out.println(course);
+//        for (Integer p : purpose) {
+//            System.out.println(p);
+//        }
+//        return "SUCCESS";
+//    }
+
     @PostMapping("/apply")
     @ResponseBody
-    public String apply(String name,String course,Integer[] purpose){
+    public String apply(@RequestParam(value = "n",defaultValue = "ANON") String name, String course, Integer[] purpose){
         System.out.println(name);
         System.out.println(course);
         for (Integer p : purpose) {
