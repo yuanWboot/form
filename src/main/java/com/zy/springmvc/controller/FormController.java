856b2f518af1854914264eb8450c054b37e8d0d1
zy <1661996964@qq.com>
2022-11-14 12:22:32 +0800

    接收表单复合数据

---
+++ src/main/java/com/zy/springmvc/controller/FormController.java
@@ -0,0 +1,20 @@
+package com.zy.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FormController {
+    @PostMapping("/apply")
+    @ResponseBody
+    public String apply(String name,String course,Integer[] purpose){
+        System.out.println(name);
+        System.out.println(course);
+        for (Integer p : purpose) {
+            System.out.println(p);
+        }
+        return "SUCCESS";
+    }
+}
