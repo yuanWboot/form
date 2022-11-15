1c4f85c9a5cc5b6f718446c80e6e1befe850caa5
zy <1661996964@qq.com>
2022-11-15 08:19:59 +0800

    使用map接收复合数据时，复合数据中只接收首个，会发生数据丢失@RequestParam Map map

--- src/main/java/com/zy/springmvc/controller/FormController.java
+++ src/main/java/com/zy/springmvc/controller/FormController.java
@@ -1,10 +1,13 @@
 package com.zy.springmvc.controller;
 
+import com.zy.springmvc.entity.Form;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 public class FormController {
@@ -19,14 +22,29 @@ public class FormController {
 //        return "SUCCESS";
 //    }
 
+//    @PostMapping("/apply")
+//    @ResponseBody
+//    public String apply(@RequestParam(value = "n",defaultValue = "ANON") String name, String course, Integer[] purpose){
+//        System.out.println(name);
+//        System.out.println(course);
+//        for (Integer p : purpose) {
+//            System.out.println(p);
+//        }
+//        return "SUCCESS";
+//    }
+
+
+//    @PostMapping("/apply")
+//    @ResponseBody
+//    public String apply(Form form){
+//        return "SUCCESS";
+//    }
+
+    //使用map接收复合数据时，复合数据中只接收首个，会发生数据丢失
     @PostMapping("/apply")
     @ResponseBody
-    public String apply(@RequestParam(value = "n",defaultValue = "ANON") String name, String course, Integer[] purpose){
-        System.out.println(name);
-        System.out.println(course);
-        for (Integer p : purpose) {
-            System.out.println(p);
-        }
+    public String apply(@RequestParam Map map){
+        System.out.println(map);
         return "SUCCESS";
     }
 }
---
+++ src/main/java/com/zy/springmvc/entity/Form.java
@@ -0,0 +1,33 @@
+package com.zy.springmvc.entity;
+
+import java.util.List;
+
+public class Form {
+    private String name;
+    private String course;
+    private List<Integer> purpose;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public List<Integer> getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(List<Integer> purpose) {
+        this.purpose = purpose;
+    }
+}
